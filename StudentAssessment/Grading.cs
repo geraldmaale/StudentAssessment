81b549fd01bf00825ea573629ccf3597aeafd7cd
Gerald Maale <55647505+geraldmaale@users.noreply.github.com>
2021-10-30 18:08:05 +0800

    Add project files.

---
+++ StudentAssessment/Grading.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAssessment
+{
+    public static class Grading
+    {
+        /// <summary>
+        /// Get grade from marks
+        /// </summary>
+        /// <param name="marks">Marks of candidate or student</param>
+        /// <returns>Returns a grade <see cref="char"/></returns>
+        public static char GetGrade(double marks)
+        {
+            if (marks >= 80)
+                return 'A';
+            else if (marks >= 70)
+                return 'B';
+            else if (marks >= 60)
+                return 'C';
+            else if (marks >= 50)
+                return 'D';
+            else if (marks >= 40)
+                return 'E';
+            else
+                return 'F';
+        }
+
+        /// <summary>
+        /// Get remarks from marks
+        /// </summary>
+        /// <param name="marks">Marks of candidate or student</param>
+        /// <returns>Returns a remarks <see cref="string"/></returns>
+        public static string GetRemarks(double marks)
+        {
+            if (marks >= 80)
+                return "Excellent";
+            else if (marks >= 70)
+                return "Very good";
+            else if (marks >= 60)
+                return "Good";
+            else if (marks >= 50)
+                return "Credit";
+            else if (marks >= 40)
+                return "Pass";
+            else
+                return "Fail";
+        }
+    }
+}
---
+++ StudentAssessment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentAssessment
+{
+    public class Program
+    {
+        private static string _name;
+        private static char _grade;
+        private static double _total;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("<<MARKS CALCULATOR>>");
+            Console.WriteLine("<<================>>");
+            Console.WriteLine("Enter your name to display your results ");
+            Console.Write("Enter Student Name: ");
+            _name = Console.ReadLine();
+            
+            Dictionary<string, double> subjectMarks = new();
+            subjectMarks.Add("English", 98.2);
+            subjectMarks.Add("Core Mathematics", 89);
+            subjectMarks.Add("Int. Science", 79);
+            subjectMarks.Add("Social Studies", 88.6);
+            subjectMarks.Add("Programming with C++:", 76);
+            subjectMarks.Add("I.T Tools", 86);
+            subjectMarks.Add("Artificial intelligence", 79);
+            subjectMarks.Add("Digital Electronics", 69.5);
+
+            Console.WriteLine("Below are your results\n");
+            Console.WriteLine("===================== ");
+            foreach (var subjectMark in subjectMarks)
+            {
+                Console.WriteLine($"{subjectMark.Key} | " +
+                                  $"Marks: {subjectMark.Value}, " +
+                                  $"Grade: {Grading.GetGrade(subjectMark.Value)}, " +
+                                  $"Remarks: {Grading.GetRemarks(subjectMark.Value)}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"Hello {_name}, your total marks for the {subjectMarks.Keys.Count} subjects is {subjectMarks.Values.Sum()}");
+
+            Console.ReadLine();
+        }
+    }
+}
